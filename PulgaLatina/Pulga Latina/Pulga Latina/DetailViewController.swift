2216d4ef12847ef1b86c0b23d52cc3d5d6446ee4
michaelgu95 <michaelgu95@gmail.com>
2014-10-19 07:56:26 -0400

    XCode Project

---
+++ PulgaLatina/Pulga Latina/Pulga Latina/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Pulga Latina
+//
+//  Created by Michael Gu on 10/18/14.
+//  Copyright (c) 2014 Gutang. All rights reserved.
+//
+
+import UIKit
+
+
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+    
+            webView.loadHTMLString(adContent, baseURL: nil)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PulgaLatina/Pulga Latina/Pulga Latina/MasterViewController.swift
@@ -0,0 +1,239 @@
+//
+//  MasterViewController.swift
+//  Pulga Latina
+//
+//  Created by Michael Gu on 10/18/14.
+//  Copyright (c) 2014 Gutang. All rights reserved.
+//
+
+
+import UIKit
+import CoreData
+
+var adContent:String = ""
+
+class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    @IBOutlet var adTableView : UITableView?
+    
+    var adData = []
+    var detailViewController: DetailViewController? = nil
+    var managedObjectContext: NSManagedObjectContext? = nil
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            self.clearsSelectionOnViewWillAppear = false
+            self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var url: NSURL = NSURL.URLWithString("http://www.pulgalatina.com/api/v1/ads/?format=json&page=2")
+        
+        var request: NSURLRequest = NSURLRequest(URL:url)
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        let session  = NSURLSession(configuration: config)
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(data, response, error)  in
+            
+            if error != nil {
+                println(error.localizedDescription)
+            }
+            
+            var err: NSError?
+            
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                
+                let results: NSArray = jsonResult["results"] as NSArray
+            
+            
+            dispatch_async(dispatch_get_main_queue(), {
+               self.adData = results
+                self.adTableView!.reloadData()
+                })
+            
+            
+            })
+        
+        task.resume()
+        
+            if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func insertNewObject(sender: AnyObject) {
+        let context = self.fetchedResultsController.managedObjectContext
+        let entity = self.fetchedResultsController.fetchRequest.entity
+        let newManagedObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name, inManagedObjectContext: context) as NSManagedObject
+             
+        // If appropriate, configure the new managed object.
+        // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
+        newManagedObject.setValue(NSDate.date(), forKey: "timeStamp")
+             
+        // Save the context.
+        var error: NSError? = nil
+        if !context.save(&error) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            //println("Unresolved error \(error), \(error.userInfo)")
+            abort()
+        }
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow() {
+          
+                let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return adData.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell :UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        let rowData: NSDictionary = self.adData[indexPath.row] as NSDictionary
+        
+        cell.textLabel?.text = rowData["name"] as? String
+    
+        cell.detailTextLabel?.text = rowData["price"] as? String
+        
+        var adImageURL =  rowData["primary_image_url"] as? String
+        
+//        var adImage =  UIImage(data: NSData(contentsOfURL: NSURL(string: adImageURL!)))
+//       
+//            cell.imageView?.image = adImage
+        
+        
+        return cell
+    }
+    
+   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let rowData: NSDictionary = self.adData[indexPath.row] as NSDictionary
+        adContent = rowData["description"] as String
+        println(adContent)
+        self.performSegueWithIdentifier("showDetail", sender: tableView)
+    }
+
+
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as NSManagedObject
+        cell.textLabel?.text = object.valueForKey("timeStamp")!.description
+    }
+
+    // MARK: - Fetched results controller
+
+    var fetchedResultsController: NSFetchedResultsController {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest = NSFetchRequest()
+        // Edit the entity name as appropriate.
+        let entity = NSEntityDescription.entityForName("Event", inManagedObjectContext: self.managedObjectContext!)
+        fetchRequest.entity = entity
+        
+        // Set the batch size to a suitable number.
+        fetchRequest.fetchBatchSize = 20
+        
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "timeStamp", ascending: false)
+        let sortDescriptors = [sortDescriptor]
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+    	var error: NSError? = nil
+    	if !_fetchedResultsController!.performFetch(&error) {
+    	     // Replace this implementation with code to handle the error appropriately.
+    	     // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+             //println("Unresolved error \(error), \(error.userInfo)")
+    	     abort()
+    	}
+        
+        return _fetchedResultsController!
+    }    
+    var _fetchedResultsController: NSFetchedResultsController? = nil
+
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.beginUpdates()
+    }
+
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        switch type {
+            case .Insert:
+                self.tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+            case .Delete:
+                self.tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+            default:
+                return
+        }
+    }
+
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath) {
+        switch type {
+            case .Insert:
+                tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Fade)
+            case .Delete:
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            case .Update:
+                self.configureCell(tableView.cellForRowAtIndexPath(indexPath)!, atIndexPath: indexPath)
+            case .Move:
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Fade)
+            default:
+                return
+        }
+    }
+
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.endUpdates()
+    }
+
+    
+    
+    /*
+     // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
+     
+     func controllerDidChangeContent(controller: NSFetchedResultsController) {
+         // In the simplest, most efficient, case, reload the table view.
+         self.tableView.reloadData()
+     }
+     */
+
+}
+
